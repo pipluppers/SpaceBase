dc7eafc85285d8b6bab4c21ec862876b63c4c0da
Miso <nguyenalex24@gmail.com>
2025-03-16 19:39:51 -0700

    Separate Escape menu into own control

---
+++ SpaceBase/SpaceBase/MainWindow/EscapeMenuControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceBase
+{
+    /// <summary>
+    /// Interaction logic for EscapeMenuControl.xaml
+    /// </summary>
+    public partial class EscapeMenuControl : UserControl
+    {
+        public EscapeMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
