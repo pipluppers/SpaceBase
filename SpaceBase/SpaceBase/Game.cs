596ba94edc53a72fb0c058249474866dd951cd82
Miso <nguyenalex24@gmail.com>
2025-03-22 00:29:41 -0700

    Cleanup

--- SpaceBase/SpaceBase/Game.cs
+++ SpaceBase/SpaceBase/Game.cs
@@ -56,7 +56,7 @@
                 Players.Add(player);
             }
 
-            CacheCards();
+            LoadCards();
         }
 
         #region Properties
@@ -212,9 +212,9 @@
         }
 
         /// <summary>
-        /// Reads the database to store all card information.
+        /// Loads all card information from the database.
         /// </summary>
-        private void CacheCards()
+        private void LoadCards()
         {
             try
             {
@@ -226,6 +226,8 @@
 
                 int i = 0;
 
+                // First load the initial cards for each player.
+
                 for (; i < Constants.MaxSectorID; i++)
                 {
                     foreach (Player player in Players)
@@ -234,6 +236,8 @@
 
                 foreach (Player player in Players)
                     player.AddCardToSectorEvent += AddCardToSectorHandler;
+
+                // Now load the decks.
 
                 for (; i < cards.Count; i++)
                 {
@@ -259,89 +263,6 @@
             {
                 Trace.WriteLine($"Error loading the cards from the database: {ex.Message}");
             }
-        }
-
-        /// <summary>
-        /// Reads environment variables and returns the SqlConnection object.
-        /// </summary>
-        /// <returns>The connection object.</returns>
-        /// <exception cref="InvalidOperationException">There was an error reading the environment variables or getting the connection.</exception>
-        private static SqlConnection GetSqlConnection()
-        {
-            string? server = Environment.GetEnvironmentVariable(Constants.ServerEnvironmentVariable, EnvironmentVariableTarget.User);
-            string? database = Environment.GetEnvironmentVariable(Constants.DatabaseEnvironmentVariable, EnvironmentVariableTarget.User);
-
-            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(database))
-                throw new InvalidOperationException($"Error loading the server {server} or database {database}.");
-
-            string connectionString = $"{Constants.ServerKey}={server};{Constants.DatabaseKey}={database};";
-
-            // TODO  Get Certificate Authority signed certificate and remove the assignment below
-            connectionString += "Encrypt=False;Trusted_Connection=True";
-
-            return new SqlConnection(connectionString);
-        }
-
-        /// <summary>
-        /// Creates a card based on the current row in the table.
-        /// </summary>
-        /// <param name="reader">The iterator over the current row in the table.</param>
-        /// <returns>A card based on the table row.</returns>
-        private static Card CreateCard(SqlDataReader reader)
-        {
-            int level = reader.GetInt32(0);
-            int sectorID = reader.GetInt32(1);
-            int cost = reader.GetInt32(2);
-            int effect = reader.GetInt32(3);
-            int effectAmount = reader.GetInt32(4);
-            int? secondaryEffectAmount = !reader.IsDBNull(5) ? reader.GetInt32(5) : null;
-            int deployedEffect = reader.GetInt32(6);
-            int deployedEffectAmount = reader.GetInt32(7);
-            int? secondaryDeployedEffectAmount = !reader.IsDBNull(8) ? reader.GetInt32(7) : null;
-
-            if (reader.IsDBNull(9))
-            {
-                return new Card(level, sectorID, cost,
-                    (ActionType)effect, effectAmount, secondaryEffectAmount,
-                    (ActionType)deployedEffect, deployedEffectAmount, secondaryDeployedEffectAmount);
-            }
-            else
-            {
-                int chargeEffect = reader.GetInt32(9);
-                int requiredChargeCubes = reader.GetInt32(10);
-                int chargeCubeLimit = reader.GetInt32(11);
-                int chargeCardType = reader.GetInt32(12);
-                int deployedChargeEffect = reader.GetInt32(13);
-                int deployedRequiredChargeCubes = reader.GetInt32(14);
-                int deployedChargeCubeLimit = reader.GetInt32(15);
-                int deployedChargeCardType = reader.GetInt32(16);
-
-                return new ChargeCard(level, sectorID, cost,
-                    (ActionType)effect, effectAmount, secondaryEffectAmount,
-                    (ActionType)deployedEffect, deployedEffectAmount, secondaryDeployedEffectAmount,
-                    (ChargeActionType)chargeEffect, requiredChargeCubes, chargeCubeLimit, (ChargeCardType)chargeCardType,
-                    (ChargeActionType)deployedChargeEffect, deployedRequiredChargeCubes, deployedChargeCubeLimit, (ChargeCardType)deployedChargeCardType);
-            }
-        }
-
-        /// <summary>
-        /// Populate the starting cards on each player's boards and subscribe to adding card events.
-        /// </summary>
-        /// <param name="reader">The reader object over the data.</param>
-        private void PopulatePlayerStartingCards(SqlDataReader reader)
-        {
-            for (int i = 0; i < 12; ++i)
-            {
-                reader.Read();
-
-                var card = CreateCard(reader);
-
-                foreach (var player in Players)
-                    player.AddCard(card);
-            }
-
-            foreach (var player in Players)
-                player.AddCardToSectorEvent += AddCardToSectorHandler;
         }
 
         /// <summary>
